f4b1af677861f41df502031dcfe63906f99fa3cb
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-14 18:00:31 +0530

    Jira SCRUM-1 Test v2

--- Jira_Verify.py
+++ Jira_Verify.py
@@ -1,14 +1,18 @@
 import subprocess
 import re
 import sys
-import requests
+#import requests
 import os
 
 def update_jira_ticket(jira_url="", jira_username="", 
                        jira_api_token="", 
-                       jira_id_pattern=r'\b[A-Z]+-\d+\b', commit_msg="SCRUM-1 first Jira"):
+                       jira_id_pattern=r'\b[A-Z]+-\d+\b', commit_msg="SCRUM-1-123-first Jira"):
     match = re.search (jira_id_pattern, commit_msg)
-    print (match)
+    print(match)
+    jira_id=match.group()
+    print(jira_id)
 
 
-    
+if __name__ == "__main__":
+   print("In Main")
+   update_jira_ticket()
