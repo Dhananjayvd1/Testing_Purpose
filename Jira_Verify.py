16ee2d1dbcb9c5952938fb6387be3746fa6e00b3
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-14 14:24:09 +0530

    SCRUM-1 Python script v1

---
+++ Jira_Verify.py
@@ -0,0 +1,14 @@
+import subprocess
+import re
+import sys
+import requests
+import os
+
+def update_jira_ticket(jira_url="", jira_username="", 
+                       jira_api_token="", 
+                       jira_id_pattern=r'\b[A-Z]+-\d+\b', commit_msg="SCRUM-1 first Jira"):
+    match = re.search (jira_id_pattern, commit_msg)
+    print (match)
+
+
+    
