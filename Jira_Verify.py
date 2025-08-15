cd7e68e5b845f468e60bfc6e8d281f3e273250b5
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-15 16:17:09 +0530

    SCRUM-v11 JIRA Test V11

--- Jira_Verify.py
+++ Jira_Verify.py
@@ -9,11 +9,11 @@ def update_jira_ticket(jira_id_pattern=r'\b[A-Z]+-\d+\b'):
     jira_username = os.getenv("jira_username")
     jira_api_token = os.getenv("jira_api_token")
     commit_msg = os.getenv("commit_msg")
-    print(commit_msg)
+    print("Commit Message fom Github Repo-", commit_msg )
     match = re.search (jira_id_pattern, commit_msg)
     print(match)
     jira_id=match.group()
-    print(jira_id)
+    print(" JIRA ID- {jira_id} ")
     print(jira_url)
     print(commit_msg)
 
