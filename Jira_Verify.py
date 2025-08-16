cf0edcd8d3d190e5117063cb45b67a85c04a81f2
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-16 17:37:57 +0530

    SCRUM-1 V25

--- Jira_Verify.py
+++ Jira_Verify.py
@@ -31,7 +31,8 @@ def update_jira_ticket(jira_id_pattern=r'\b[A-Z]+-\d+\b'):
 
     print("After JIRA===")
     print(jira.get_all_projects)
-
+    print(jira.get_project("SCRUM"))
+    print(jira.get_issue("SCRUM-1"))
     #jira_client = JIRA(options={'server': "https://dhananjayvd1.atlassian.net/" }, basic_auth=("dhananjayvd1@gmail.com", "Jan_2025"))
     #print("JIRA Client OP-", jira_client.project('SCRUM'))
    # issue = jira_client.issue(jira_id)
