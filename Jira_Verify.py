5bdde705d62682080017d39aa0045c7e0507b0ca
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-25 12:38:31 +0530

    SCRUM-1 Test 34

--- Jira_Verify.py
+++ Jira_Verify.py
@@ -30,7 +30,7 @@ def update_jira_ticket(jira_id_pattern=r'\b[A-Z]+-\d+\b'):
     jira_comment_url = f"{jira_url}/browse/{jira_id}/comment"
     headers = {'Content-Type': 'applcation/json'}
     payload = {'bdoy': commit_msg}
-    response_comment = requests.post(jira_comment_url, auth=(jira_username,jira_api_token) )
+    response_comment = requests.post(jira_comment_url, auth=(jira_username,jira_api_token), headers= headers, json=payload )
     print(response_comment.status_code)
     
 
