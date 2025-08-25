17852d1c1e671693f3ff248de2e89e114d1bb137
Dhananjayvd1 <dhananjayvd1@gmail.com>
2025-08-25 12:22:11 +0530

    SCRUM-1 JIRA test V32

--- Jira_Verify.py
+++ Jira_Verify.py
@@ -22,7 +22,8 @@ def update_jira_ticket(jira_id_pattern=r'\b[A-Z]+-\d+\b'):
     #https://dhananjayvd1.atlassian.net/browse/SCRUM-1
     jira_api_url = f"{jira_url}/browse/{jira_id}"
     print(jira_api_url)
-    response_verify = requests.get(jira_api_url, auth=("dhananjayvd1@gmail.com","Jan_2025"))
+    #response_verify = requests.get(jira_api_url, auth=("dhananjayvd1@gmail.com","Jan_2025"))
+    response_verify = requests.get(jira_api_url, auth=(jira_username,jira_api_token))
     print(response_verify.json)
     print(response_verify.status_code)
 
